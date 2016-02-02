14712f93f5d52d9d1c3a5453b0124d7a02b60d42
Yosemite Retail <yosemiteretail@Yosemites-MacBook-Pro.local>
2016-02-02 13:38:40 -0200

    Initial Commit

---
+++ PayPalTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PayPalTest
+//
+//  Created by Yosemite Retail on 02/02/16.
+//  Copyright (c) 2016 andrei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ PayPalTest/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  PayPalTest
+//
+//  Created by Yosemite Retail on 02/02/16.
+//  Copyright (c) 2016 andrei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
